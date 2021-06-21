d2671c0378ab09666a381421f59b2e7d70806610
Mogan Xumerle <moganrz@gmail.com>
2021-06-21 08:55:49 -0300

    feat: remove configuration use developer exception page

--- Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura.App/Startup.cs
@@ -13,7 +13,7 @@ namespace Alura.ListaLeitura.App
 
         public void Configure(IApplicationBuilder app)
         {
-            app.UseDeveloperExceptionPage();
+            //app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
 
             // var routeBuilder = new RouteBuilder(app);
